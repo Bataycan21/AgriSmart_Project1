396ebc5a72fce24c9739f50d896a95e2bdb0b10a
Bataycan21 <zylbataycan@gmail.com>
2026-03-09 17:37:12 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -54,7 +54,7 @@ function renderShell(activeId) {
 
       <div class="main-area">
         <header class="top-header">
-          <p class="header-welcome">Welcome back, <span>Juan Dela Cruz</span></p>
+          <p class="header-welcome">Welcome back, <span>${currentUser.name}</span></p>
           <div class="header-right">
             <button class="notif-btn">
               <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/></svg>
